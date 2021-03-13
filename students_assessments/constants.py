9f28795e944e26847f9da77f560366d983d3a3fb
aditipancholi <adipancholi007@gmail.com>
2021-03-13 04:50:22 -0500

    # Update

--- students_assessments/constants.py
+++ students_assessments/constants.py
@@ -9,7 +9,7 @@ DATE_TIME = datetime.now().strftime(DATE_TIME_FORMAT)
 CLASS_DATA_FILES_PATH = os.environ.get('FILES_PATH', r'assets')
 CSV_EXTENSION = '.csv'
 
-OUTPUT_PATH = r'output'
+OUTPUT_PATH = os.path.join(ROOT_DIR, '..', r'output')
 OUT_PUT_FILE_NAME = r'Automated Report_{date_time}.txt'.format(date_time=DATE_TIME)
 OUTPUT_FILE_NAME = os.environ.get('OUTPUT_FILES_PATH', os.path.join(OUTPUT_PATH, OUT_PUT_FILE_NAME))
 
